b6613ef0b26377def870fde977902687020a7b97
razan111 <razan15-12966@diu.edu.bd>
2023-03-09 23:47:16 +0600

    responsive done

--- js/Carousel.js
+++ js/Carousel.js
@@ -1,41 +1,62 @@
-$('#product-carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 4
+$(document).ready(function () {
+
+    // Add scrollspy to <body>
+    $('body').scrollspy({ target: ".navbar-nav", offset: 67 });
+
+    $(".navbar-nav a").on('click', function(event) {
+        if (this.hash !== ""){
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+            $(".navbar-collapse").collapse("hide");
         }
-    }
+    })
+    
+    $('#product-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+
+    // testimonial-carousel
+
+    $('#testimonial-carousel').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    })
 })
 
-// testimonial-carousel
-
-$('#testimonial-carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 2
-        }
-    }
-})
